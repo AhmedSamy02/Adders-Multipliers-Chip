b4912e2b534f0d3e00795315e26e3b07f131a059
AhmedSamy02 <https://github.com/AhmedSamy02>
2023-11-27 08:33:13 +0200

    Ripple and Normal Adders

---
+++ full_adder.v
@@ -0,0 +1,10 @@
+module FullAdder (
+    input  wire a,
+    input  wire b,
+    input  wire cin,
+    output reg  sum,
+    output reg  cout
+);
+  assign sum  = a ^ b ^ cin;
+  assign cout = a & b | b & cin | cin & a;
+endmodule
