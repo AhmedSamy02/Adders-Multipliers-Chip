9be01557956e0f4deca028d261c28ee8b67f4615
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-12-10 16:12:10 +0200

    tb for fpa

--- fpa.v
+++ fpa.v
@@ -3,17 +3,18 @@ module fpa (
     input  wire [31:0] in2,
     output reg  [31:0] out
 );
+integer i;
   reg  [22:0] inShift;
   reg  [ 4:0] shamt;
   wire [22:0] outShift;
   reg [22:0] A_Mantissa, B_Mantissa, Out_Mantissa;
   reg [7:0] A_Exponent, B_Exponent, Out_Exponent;
   reg choosen;
-  reg [1:0] temp;
+  reg  temp;
   reg A_Sign, B_Sign, Out_Sign;
   reg [22:0] outComp;
   BarrelShifter bs (
-      .In({1'b1, inShift}),
+      .In({inShift}),
       .Out({outShift}),
       .Shift(shamt)
   );
@@ -24,9 +25,14 @@ module fpa (
     B_Sign = in2[31];
     A_Exponent = in1[30:23];
     B_Exponent = in2[30:23];
+    outComp = 0;
+    Out_Mantissa = 0;
+    Out_Exponent = 0;
+    Out_Sign = 0;
     inShift = A_Mantissa;
     shamt = B_Exponent - A_Exponent;
     choosen = 1;
+    temp = 0;
     if (A_Exponent > B_Exponent) begin
       inShift = B_Mantissa;
       shamt   = A_Exponent - B_Exponent;
@@ -36,37 +42,61 @@ module fpa (
       Out_Sign = A_Sign;
       if (choosen == 1) begin
         Out_Exponent = B_Exponent;
-        Out_Mantissa = outShift + B_Mantissa;
+        {temp,Out_Mantissa} = outShift + B_Mantissa;
       end
       if (choosen == 0) begin
         Out_Exponent = A_Exponent;
-        Out_Mantissa = outShift + A_Mantissa;
+        {temp,Out_Mantissa} = outShift + A_Mantissa;
       end
-
     end
     if (A_Sign != B_Sign) begin
       if (choosen == 1) begin
         if (A_Sign == 1) begin
-          outComp <= 0 - outShift;
+          outComp = 0 - outShift;
           Out_Mantissa = outComp + B_Mantissa;
         end
         if (B_Sign == 1) begin
-          outComp <= 0 - B_Mantissa;
+          outComp = 0 - B_Mantissa;
           Out_Mantissa = outComp + outShift;
         end
         Out_Exponent = B_Exponent;
       end
       if (choosen == 0) begin
         if (B_Sign == 1) begin
-          outComp <= 0 - outShift;
+          outComp = 0 - outShift;
           Out_Mantissa = outComp + A_Mantissa;
         end
         if (A_Sign == 1) begin
-          outComp <= 0 - A_Mantissa;
+          outComp = 0 - A_Mantissa;
           Out_Mantissa = outComp + outShift;
         end
         Out_Exponent = A_Exponent;
       end
+    end
+    if (temp == 1) begin
+      Out_Mantissa = Out_Mantissa >> 1;
+     Out_Mantissa[22]=1;
+
+      Out_Exponent = Out_Exponent + 1;
+    end
+
+    if( |Out_Mantissa == 1)begin
+    for ( i=22 ; i>-1 ; i=i-1) begin
+
+      if (Out_Mantissa[i] == 1) begin
+        i = 0;
+      end
+
+        else begin
+        Out_Mantissa = Out_Mantissa << 1;
+        Out_Exponent = Out_Exponent - 1; 
+    end
+
+    end
+
+    end
+    else begin
+        Out_Exponent = 0;
     end
     out = {Out_Sign, Out_Exponent, Out_Mantissa};
   end
---
+++ fpa_tb.v
@@ -0,0 +1,91 @@
+`timescale 1 ns / 100 ps
+module fpa_tb ();
+reg [31:0] A;
+reg [31:0] B;
+wire [31:0] Sum;
+fpa fpa (
+    .in1(A),
+    .in2(B),
+    .out(Sum)
+);
+integer success_count;
+initial begin 
+
+success_count = 0;
+A = 32'b00000001110011100000000000000000; B = 32'b00000010010010100100000000000000; //9.75 + 18.5625
+#10; 
+if (Sum == 32'b00000010011100010100000000000000) 
+    begin
+    $display("TestCase#1: success");
+    success_count = success_count + 1;
+    end 
+    else 
+    begin
+    $display("TestCase#1: failed with input A=%0b, B=%0b and Output Sum=%0d ", A, B, Sum);
+    end
+///////////////////////////
+A = 32'b00000001110011100000000000000000; B = 32'b10000010010010100100000000000000; //9.75 + (-18.5625)
+#10; 
+if (Sum == 32'b10000001110001101000000000000000) 
+    begin
+    $display("TestCase#2: success");
+    success_count = success_count + 1;
+    end 
+    else 
+    begin
+    $display("TestCase#2: failed with input A=%0b, B=%0b and Output Sum=%0d ", A, B, Sum);
+    end
+A = 32'b10000001110011100000000000000000; B = 32'b10000010010010100100000000000000; // -9.75 + (-18.5625)
+#10;
+if (Sum == 32'b10000010011100010100000000000000) begin
+  $display("TestCase#3: success");
+  success_count = success_count + 1;
+end else begin
+  $display("TestCase#3: failed with input A=%0b, B=%0b and Output Sum=%0d ", A, B, Sum);
+end
+
+A = 32'b00000001111111111111111111111111; B = 32'b00000001111111111111111111111111; //15.999999 + 15.999999 
+#10; 
+if (Sum == 32'b00000010011111111111111111111111) begin
+  $display("TestCase#4: success");
+  success_count = success_count + 1;
+end else begin
+  $display("TestCase#4: failed with input A=%0b, B=%0b and Output Sum=%0d ", A, B, Sum);
+end
+
+A = 32'b10000001111111111111111111111111; B = 32'b10000001111111111111111111111111; // -15.999999 + (-15.999999)
+#10; 
+if (Sum == 32'b10000010011111111111111111111111) begin
+  $display("TestCase#5: success");
+  success_count = success_count + 1;
+end else begin
+  $display("TestCase#5: failed with input A=%0b, B=%0b and Output Sum=%0d ", A, B, Sum);
+end
+A = 32'b00000000000000000000000000000000; B = 32'b00000000000000000000000000000000;  //0 + 0
+#10;
+if (Sum == 32'b00000000000000000000000000000000) begin
+  $display("TestCase#6: success");
+  success_count = success_count + 1;
+end else begin
+  $display("TestCase#6: failed with input A=%0b, B=%0b and Output Sum=%0d ", A, B, Sum);
+end
+
+A = 32'b00000001010101000000000000000000; B = 32'b00000000011000000000000000000000;  // 5.25 + 1.5
+#10; 
+if (Sum == 32'b00000001011011000000000000000000) begin
+  $display("TestCase#7: success");
+  success_count = success_count + 1;
+end else begin
+  $display("TestCase#7: failed with input A=%0b, B=%0b and Output Sum=%0d ", A, B, Sum);
+end
+A = 32'b00000001110010010000000000000000; B = 32'b00000001011110100000000000000000;  // 9.125 + (-7.625)
+#10; 
+if (Sum == 32'b00000000011000000000000000000000) begin
+  $display("TestCase#8: success");
+  success_count = success_count + 1;
+end else begin
+  $display("TestCase#8: failed with input A=%0b, B=%0b and Output Sum=%0d ", A, B, Sum);
+end
+
+end
+endmodule
