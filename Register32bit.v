e5d140778b237c7801e32b07b7b54fc60a4c0605
AhmedSamy02 <https://github.com/AhmedSamy02>
2023-12-11 20:16:53 +0200

    SimpleMultiplier with tb

---
+++ Register32bit.v
@@ -0,0 +1,16 @@
+module Register32bit (
+    input wire clk,
+    input wire rst,
+    input wire [31:0] in,
+    output reg [31:0] out
+);
+
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      out <= 32'b0;
+    end else begin
+      out <= in;
+    end
+  end
+
+endmodule
---
+++ SimpleMultiplier.v
@@ -0,0 +1,32 @@
+module SimpleMultiplier (
+    input wire [31:0] in1,
+    input wire [31:0] in2,
+    input wire clk,
+    input wire rst,
+    output wire [31:0] out
+);
+  wire [31:0] A;
+  wire [31:0] B;
+  reg [30:0] product;
+  reg sign;
+  Register32bit registerIn1 (
+      .clk(clk),
+      .rst(rst),
+      .in (in1),
+      .out(A)
+  );
+  Register32bit registerIn2 (
+      .clk(clk),
+      .rst(rst),
+      .in (in2),
+      .out(B)
+  );
+  Register32bit registerOut (
+      .clk(clk),
+      .rst(rst),
+      .in ({sign, product}),
+      .out(out)
+  );
+  assign product = A[30:0] * B[30:0];
+  assign sign = |product && (A[31] ^ B[31]);
+endmodule
