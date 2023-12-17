095b924514946f483c161a1c91991fa600caee76
AhmedSamy02 <https://github.com/AhmedSamy02>
2023-12-18 00:35:23 +0200

    Syntheziable FPM and Removing Latches

--- fpm.v
+++ fpm.v
@@ -9,7 +9,13 @@ module fpm (
   reg [47:0] temp;
   reg [7:0] temp_exp;
   integer i;
+  reg finished;
   always @(*) begin
+    temp = 0;
+    temp_exp = 0;
+    i=0;
+    finished = 0;
+    additionalout=0;
     if (additional1 == 1 && additional2 == 1) begin
       temp = {additional1, in1[22:0]} * {additional2, in2[22:0]};
       temp_exp = in1[30:23] + in2[30:23] - 127;
@@ -22,11 +28,14 @@ module fpm (
         temp_exp = temp_exp + 1;
       end
       for (i = 0; i < 48; i = i + 1) begin
-        temp = temp << 1;
-        if (temp[46] == 1) begin
-          i = 47;
-        end else begin
-          temp_exp = temp_exp - 1;
+        temp = temp + 0;
+        if (finished == 0) begin
+          temp = temp << 1;
+          if (temp[46] == 1) begin
+            finished = 1'b1;
+          end else begin
+            temp_exp = temp_exp - 1;
+          end
         end
       end
       out[22:0] = temp[47:25];
